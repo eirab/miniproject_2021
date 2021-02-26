fae2041bc28ad741ee9e8f79756d11becfa28ce0
Eira Birkhammar <eirabirkhammar@gmail.com>
2021-02-26 15:02:44 +0100

    d

--- game.c
+++ game.c
@@ -14,6 +14,8 @@
 #include <stdint.h>   /* Declarations of uint_32 and the like */
 #include "pic32mx.h"  /* Declarations of system-specific addresses etc */
 
+void *stdin, *stdout, *stderr;
+
 
 
 
