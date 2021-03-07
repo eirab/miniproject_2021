eb6ab0bcbc1b3c2256e5d6d076fc90012da7eec0
eirab <31371140+eirab@users.noreply.github.com>
2021-03-07 16:23:22 +0100

    Added comments

--- controller.c
+++ controller.c
@@ -1,4 +1,9 @@
 
+/*------- controller.c - functions relating to hardware configuration -------*/
+/* Contains functions to configure Timer 2, input/output pins and so on 
+/* Based on pre-existing code, added to by Eira Birkhammar and Viktor Borg */
+
+
 #include <stdint.h>   
 #include "ground.h"
 #include "pic32mx.h"
@@ -8,13 +13,13 @@
 
 
 
-  /*
-	  This will set the peripheral bus clock to the same frequency
-	  as the sysclock. That means 80 MHz, when the microcontroller
-	  is running at 80 MHz. Changed 2017, as recommended by Axel.
-	*/
+/* Set the peripheral bus clock to the same frequency
+as the sysclock. That means 80 MHz, when the microcontroller
+is running at 80 MHz. Changed 2017, as recommended by Axel.
+*/
 void configurePBClock(){
     
+	/* PRE-EXISTING CODE */
 	SYSKEY = 0xAA996655;  /* Unlock OSCCON, step 1 */
 	SYSKEY = 0x556699AA;  /* Unlock OSCCON, step 2 */
 	while(OSCCON & (1 << 21)); /* Wait until PBDIV ready */
@@ -23,6 +28,8 @@ void configurePBClock(){
 	SYSKEY = 0x0;  /* Lock OSCCON */
 }
 
+/* Configures pin values 
+PRE-EXISTING CODE */
 void configurePins(){
 
 	/* Set up output pins */
@@ -47,23 +54,28 @@ void configurePins(){
 	TRISECLR = 0xFF;
 	PORTECLR = 0xFF;
 }
-
+/* Configures Timer 2 to generate an interrupt 
+every 1/10 of a second 
+Authored by Eira Birkhammar */
 void enable_timer2(){
     
- 	T2CONSET = 0x70;              //Set prescale to 256, prescale < 256 will generate int requiring more than 16 bits
-    PR2 = (80000000 / 256) / 10;  //Set period to 1/10 of a second
-    TMR2 = 0;                     //Clear tmr register
-    T2CONSET = 0x8000;            // turn timer on
-    IEC(0) = IEC(0) | 0x100;    //Set bit 8 to 1 in Interrupt Enable Control Register, T2IE = interrupt enable bit 
-    IPC(2) = IPC(2) | 0x1F;    //Set interrupt priorities to highest setting because lecture 6 said so
+    T2CONSET = 0x70;              /* Set prescalar to 256 */
+    PR2 = (80000000 / 256) / 10;  /* Set period value to cause an interrupt every 1/10 of a second*/
+    TMR2 = 0;                     /* Clear TMR2 register  */
+    IEC(0) = IEC(0) | 0x100;      /* Enable interrupts */
+    IPC(2) = IPC(2) | 0x1F;       /* Set interrupt priority to highest */
+    T2CONSET = 0x8000;            /* Turn timer on */
     
 }
-
+/* Enables interrupts globally
+Written by Eira Birkhammnar */
 void enableGlobalInterrupts(){
 
     enable_interrupt();       //Call enable interrupt that is in gameloop.S
 }
 
+/* Disables Timer 2
+Written by Viktor Borg */
 void disable_timer2(){
     T2CONCLR = 0x8000;
 }
