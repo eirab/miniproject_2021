d7edf4fb89a6b21c4d87664cabbd57b6175d2968
eirab <31371140+eirab@users.noreply.github.com>
2021-03-07 16:05:41 +0100

    Removed unused declarartions

--- assets.h
+++ assets.h
@@ -4,10 +4,9 @@
 
 /* Declare bitmap array containing font */
 extern const uint8_t const font[128*8];
-/* Declare bitmap array containing icon */
-extern const uint8_t const icon[128];
-extern const uint8_t const snopp[128];
 char textbuffer[4][16];
+
+/* Added by Viktor Borg and Eira Birkhammar */
 const uint8_t const font[];
 const uint8_t spaceship[6];
 const uint8_t monster[8];
