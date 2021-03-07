39ea355fed5e22c8e843d5b2bf857ea9e860034b
eirab <31371140+eirab@users.noreply.github.com>
2021-03-07 15:50:15 +0100

    Added comments

--- spaceship.h
+++ spaceship.h
@@ -1,9 +1,24 @@
+/*--- spaceship.h -----*/
+/* Defines the characteristics 
+* of the spaceship 
+
+* Author: Eira Birkhammar
+*/
+
 #include <stdint.h>
 #ifndef SPACESHIP_H
 #define SPACESHIP_H
 
-
-
+/* xPos - current position of the spaceship
+* along the x-axis 
+*
+* page_pos - current page position, written 
+* for an 128x32 OLED where the display is 
+* divided into 4 horizontal pages
+*
+* Refer to the reference manual for the display
+* controller: https://cdn-shop.adafruit.com/datasheets/SSD1306.pdf
+*/
 typedef struct Spaceship
 {
     int xPos;
@@ -14,3 +29,4 @@ typedef struct Spaceship
 
 
 #endif
+ https://cdn-shop.adafruit.com/datasheets/SSD1306.pdf
